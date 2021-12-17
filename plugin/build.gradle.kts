4fa05c0899a731447d0cbe55914ef47925fe2cd9
Alphonse Bendt <alphonse.bendt@gmail.com>
2021-12-17 18:58:11 +0100

    replace explicit dependency on kotlin-runtime with gradleApi()

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -24,11 +24,7 @@ repositories {
 }
 
 dependencies {
-    // Align versions of all Kotlin components
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-
-    // Use the Kotlin JDK 8 standard library.
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation(gradleApi())
 
     implementation("net.sourceforge.plantuml:plantuml:1.2021.16")
 
