1e4532e9f316e74fc74adc984e7798f0dd720ee5
Alphonse Bendt <alphonse.bendt@gmail.com>
2022-02-20 15:19:28 +0100

    additional task that allows to convert a whole directory

--- plugin/src/functionalTest/kotlin/io/github/redgreencoding/plantuml/PlantumlGradlePluginFunctionalSpec.kt
+++ plugin/src/functionalTest/kotlin/io/github/redgreencoding/plantuml/PlantumlGradlePluginFunctionalSpec.kt
@@ -18,8 +18,10 @@ import javax.xml.validation.Validator
  * A simple functional test for the 'io.github.redgreencoding.plantuml.greeting' plugin.
  */
 class PlantumlGradlePluginFunctionalSpec : StringSpec({
-    val gradleDSL =
-        """
+
+    fun usingExtension() {
+        val gradleDSL =
+            """
             plugins {
                 id('io.github.redgreencoding.plantuml')
             }
@@ -39,10 +41,10 @@ class PlantumlGradlePluginFunctionalSpec : StringSpec({
             }
         """
 
-    include(configurePlantumlBuild("gradle", "", gradleDSL))
+        include(canConfigurePlantUmlPlugin("gradleExtension", "", gradleDSL))
 
-    val kotlinDSL =
-        """
+        val kotlinDSL =
+            """
             plugins {
                 id("io.github.redgreencoding.plantuml")
             }
@@ -62,10 +64,58 @@ class PlantumlGradlePluginFunctionalSpec : StringSpec({
             }
         """
 
-    include(configurePlantumlBuild("kotlin", ".kts", kotlinDSL))
+        include(canConfigurePlantUmlPlugin("kotlinExtension", ".kts", kotlinDSL))
+    }
+
+    fun usingTasks() {
+
+        val gradleDSL =
+            """
+            import io.github.redgreencoding.plantuml.GenerateDiagramsTask
+             
+            plugins {
+                id('io.github.redgreencoding.plantuml')
+            }
+            
+            plantuml {
+                options {
+                    outputDir = project.file("svg")
+                }
+            }
+                        
+            tasks.register('generateDiagrams', GenerateDiagramsTask) {
+                source = fileTree(dir: 'puml')
+            }
+        """
+
+        include(canUseGlobPattern("gradleTasks", "", gradleDSL))
+
+        val kotlinDSL =
+            """
+            import io.github.redgreencoding.plantuml.GenerateDiagramsTask
+            
+            plugins {
+                id("io.github.redgreencoding.plantuml")
+            }
+            
+            plantuml {
+                options {
+                    outputDir = project.file("svg")
+                }
+            }
+            
+            tasks.register<GenerateDiagramsTask>("generateDiagrams") {
+                source.set(fileTree("puml"))    
+            }
+        """
+        include(canUseGlobPattern("kotlinTasks", ".kts", kotlinDSL))
+    }
+
+    usingExtension()
+    usingTasks()
 })
 
-private fun configurePlantumlBuild(name: String, extension: String, buildFile: String) =
+private fun canConfigurePlantUmlPlugin(name: String, extension: String, buildFile: String) =
     stringSpec {
         "can configure plantuml task using $name DSL" {
             // Setup the test build
@@ -109,6 +159,69 @@ private fun configurePlantumlBuild(name: String, extension: String, buildFile: S
             }
         }
     }
+
+
+private fun canUseGlobPattern(name: String, extension: String, buildFile: String) =
+    stringSpec {
+        "can use Glob pattern using $name DSL" {
+            // Setup the test build
+            val projectDir = File("build/functionalTest/$name")
+            projectDir.mkdirs()
+            projectDir.resolve("settings.gradle$extension").writeText("")
+            projectDir.resolve("build.gradle$extension").writeText(buildFile)
+            projectDir.resolve("gradle.properties").writeText("org.gradle.unsafe.configuration-cache=true")
+
+            val pumlDir = File(projectDir, "puml")
+            pumlDir.mkdirs()
+            pumlDir.resolve("Hello.puml").writeText(
+                """
+            @startuml
+
+            Bob->Alice : Hello World!
+
+            @enduml
+                """.trimIndent()
+            )
+
+            val pumlSubDir = File(pumlDir, "sub")
+            pumlSubDir.mkdirs()
+
+            pumlDir.resolve("Hello2.puml").writeText(
+                """
+            @startuml
+
+            Bob->Alice : Hello World!
+
+            @enduml
+                """.trimIndent()
+            )
+
+
+            // Run the build
+            val runner = GradleRunner.create().apply {
+                forwardOutput()
+                withPluginClasspath()
+                withArguments("--rerun-tasks", "generateDiagrams", "--stacktrace")
+                withProjectDir(projectDir)
+            }
+
+            val result = runner.build()
+
+            listOf(":generateDiagrams").forEach {
+                val task = result.task(it)
+                task.shouldNotBeNull()
+                task.outcome shouldBe TaskOutcome.SUCCESS
+            }
+
+            listOf("Hello.svg", "Hello2.svg").forEach {
+                val svg = File(projectDir, "svg/$it")
+                svg.shouldExist()
+
+                validateSVG(svg)
+            }
+        }
+    }
+
 
 private fun validateSVG(file: File) {
     file.inputStream().buffered().use {
